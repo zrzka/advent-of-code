09ec21e66ddf898a23b6ce3185d3dd0c847ebd23
Robert Vojta <rvojta@me.com>
2022-01-02 11:49:39 +0100

    Day 5, part 2

--- lib/y2021/day05.ex
+++ lib/y2021/day05.ex
@@ -14,6 +14,16 @@ defmodule Aoc.Y2021.Day05 do
     part1 =
       lines
       |> Enum.reduce(%{}, fn {p1, p2}, acc ->
+        hv_line_points(p1, p2)
+        |> Enum.reduce(acc, fn p, acc ->
+          Map.update(acc, p, 1, &(&1 + 1))
+        end)
+      end)
+      |> Enum.count(fn {_, value} -> value > 1 end)
+
+    part2 =
+      lines
+      |> Enum.reduce(%{}, fn {p1, p2}, acc ->
         line_points(p1, p2)
         |> Enum.reduce(acc, fn p, acc ->
           Map.update(acc, p, 1, &(&1 + 1))
@@ -21,7 +31,19 @@ defmodule Aoc.Y2021.Day05 do
       end)
       |> Enum.count(fn {_, value} -> value > 1 end)
 
-    {part1, 0}
+    {part1, part2}
+  end
+
+  defp hv_line_points({x, y1}, {x, y2}) do
+    Enum.map(y1..y2, &{x, &1})
+  end
+
+  defp hv_line_points({x1, y}, {x2, y}) do
+    Enum.map(x1..x2, &{&1, y})
+  end
+
+  defp hv_line_points(_, _) do
+    []
   end
 
   defp line_points({x, y1}, {x, y2}) do
@@ -32,9 +54,21 @@ defmodule Aoc.Y2021.Day05 do
     Enum.map(x1..x2, &{&1, y})
   end
 
-  defp line_points(_, _) do
-    []
+  defp line_points({x1, y1} = p1, {x2, y2} = p2) do
+    x_inc = increment(x2 - x1)
+    y_inc = increment(y2 - y1)
+    diagonal_line_points(p1, {x_inc, y_inc}, p2)
   end
+
+  defp diagonal_line_points(p, _, p), do: [p]
+
+  defp diagonal_line_points({x1, y1}, {x_inc, y_inc} = increment, p2) do
+    [{x1, y1} | diagonal_line_points({x1 + x_inc, y1 + y_inc}, increment, p2)]
+  end
+
+  defp increment(x) when x < 0, do: -1
+
+  defp increment(x) when x > 0, do: 1
 
   defp parse(path) do
     File.stream!(path)
--- test/y2021/day05_test.exs
+++ test/y2021/day05_test.exs
@@ -5,7 +5,7 @@ defmodule Aoc.Y2021.Day05Test do
   test "verify results" do
     {p1, p2} = Aoc.Y2021.Day05.results("inputs/2021/day05.txt")
 
-    assert p1 == 0
-    assert p2 == 0
+    assert p1 == 8111
+    assert p2 == 22088
   end
 end
